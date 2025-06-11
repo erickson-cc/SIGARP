196224183a7d64e6605a274a5ba9ef7c824a9ad0
Ayshathayna <ayshathayna@gmail.com>
2025-06-11 11:05:48 -0300

    feat: Adicionando tela de cadastro de itens
    1-Criação da tela de cadastro de item
    2-Criação do arquivo JS para a tela de cadastro de item
    3-Alterações na div do menu com foco na responsividade do sistema

---
+++ src/front_end/js/script_cadastro_item.js
@@ -0,0 +1,48 @@
+
+// Adiciona evento de clique no botão do menu (ícone de "hambúrguer")
+document.getElementById("toggle_menu").addEventListener("click", function () {
+  const menu = document.querySelector(".menu_lateral"); // Seleciona o menu lateral
+  const icone = this.querySelector("i");  // Seleciona o ícone dentro do botão
+
+  menu.classList.toggle("reduzido");// Alterna a classe "reduzido" para abrir/fechar o menu
+
+ // Troca o ícone dependendo do estado do menu
+  if (menu.classList.contains("reduzido")) {
+    icone.classList.remove("bx-menu");
+    icone.classList.add("bx-menu-alt-right"); // ícone diferente para indicar recolhido
+  } else {
+    icone.classList.remove("bx-menu-alt-right");
+    icone.classList.add("bx-menu");
+  }
+});
+// Evento de clique no avatar do usuário (ícone com nome)
+document.querySelector(".usuario_menu").addEventListener("click", function (event) {
+  let popup = document.getElementById("popup"); // Referência ao pop-up de logout
+
+  // Alterna entre abrir e fechar o pop-up
+  if (popup.style.display === "block") {
+    closePopup();// Fecha o popup se já estiver aberto
+  } else {
+    showPopup();// Abre o popup se estiver fechado
+  }
+
+  // Impede que o clique no usuário feche o pop-up imediatamente
+  event.stopPropagation();
+});
+
+// Fecha o popup quando clicar fora dele
+document.addEventListener("click", function (event) {
+  let popup = document.getElementById("popup");
+  // Se o popup estiver aberto e o clique não for dentro dele, fecha o popup
+  if (popup.style.display === "block" && !popup.contains(event.target)) {
+    closePopup();
+  }
+});
+
+function showPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
